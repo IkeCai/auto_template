b1e2cd0214406f8b100616658436c551ba578c53
IKE Cai <ikecai@IKEs-MacBook-Pro.local>
2024-08-03 00:18:41 -0400

    inital jave setup

---
+++ src/main/java/com/mycompany/autotemplate/AutoTemplateApplication.java
@@ -0,0 +1,11 @@
+package com.mycompany.autotemplate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoTemplateApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AutoTemplateApplication.class, args);
+    }
+}
